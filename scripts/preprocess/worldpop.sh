e49ef95eb4af28e6749df343a0c027188a595d71
Tom Russell <tomalrussell@gmail.com>
2021-12-09 17:01:41 +0000

    Download Worldpop

---
+++ scripts/preprocess/worldpop.sh
@@ -0,0 +1,6 @@
+wget https://data.worldpop.org/GIS/Population/Global_2000_2020/2020/JAM/jam_ppp_2020.tif \
+  --directory-prefix=./incoming_data/worldpop
+wget https://data.worldpop.org/GIS/Population/Global_2000_2020/2011/JAM/jam_ppp_2011.tif \
+  --directory-prefix=./incoming_data/worldpop
+wget https://data.worldpop.org/GIS/Population/Global_2000_2020/2001/JAM/jam_ppp_2001.tif \
+  --directory-prefix=./incoming_data/worldpop
