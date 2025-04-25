4e3e3e8813c88de8d666be752ec7cc9cb9bce9cb
Fred Thomas <fred.thomas@eci.ox.ac.uk>
2025-04-25 16:52:51 +0100

    Tidy script imports

--- scripts/transport_model/roads_bridges_failure_analysis.py
+++ scripts/transport_model/roads_bridges_failure_analysis.py
@@ -4,14 +4,18 @@
 import sys
 import os
 
-import pandas as pd
-import geopandas as gpd
-import numpy as np
-import igraph as ig
 import ast
-from utils import *
-import transport_flow_and_failure_functions as tf
+import geopandas as gpd
+import igraph as ig
+import pandas as pd
 from tqdm import tqdm
+
+from jamaica_infrastructure.transport.utils import (
+    load_config,
+    map_nearest_locations_and_create_lines,
+    network_od_paths_assembly
+)
+import jamaica_infrastructure.transport.flow as tf
 
 tqdm.pandas()
 epsg_jamaica = 3448
