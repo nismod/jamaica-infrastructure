98ab10f48cc956a5fd84d5ff1eaae66327e73fbc
Fred Thomas <fred.thomas@eci.ox.ac.uk>
2025-05-18 17:39:41 +0100

    Relocate function to calling script

--- src/jamaica_infrastructure/analysis/utils.py
+++ src/jamaica_infrastructure/analysis/utils.py
@@ -1,11 +1,6 @@
 from typing import Any
 
 import pandas as pd
-from scipy import integrate
-import numpy as np
-from tqdm import tqdm
-
-tqdm.pandas()
 
 
 def is_sole_value(series: pd.Series, value: Any) -> bool:
@@ -29,50 +24,3 @@ def get_asset(network_csv_path: str, asset_gpkg: str, asset_layer: str) -> pd.Se
     elif len(row) > 1:
         raise ValueError(f"Multiple assets found for gpkg={asset_gpkg} and layer={asset_layer}")
     return row.squeeze()
-
-
-def risks(
-    dataframe,
-    index_columns,
-    probabilities,
-    expected_risk_column,
-    flood_protection_period=0,
-    flood_protection_name=None,
-):
-    """
-    Organise the dataframe to pivot with respect to index columns
-    Find the expected risks
-    """
-    if flood_protection_name is None and flood_protection_period == 0:
-        # When there is no flood protection at all
-        expected_risk_column = f"{expected_risk_column}_undefended"
-        probability_columns = [str(p) for p in probabilities]
-
-    elif flood_protection_period > 0:
-        if flood_protection_name is None:
-            expected_risk_column = (
-                f"{expected_risk_column}_{flood_protection_period}_year_protection"
-            )
-        else:
-            expected_risk_column = f"{expected_risk_column}_{flood_protection_name}"
-
-        probabilities = [
-            pr for pr in probabilities if pr <= 1.0 / flood_protection_period
-        ]
-        probability_columns = [str(p) for p in probabilities]
-    else:
-        # When there is no flood protection at all
-        expected_risk_column = f"{expected_risk_column}_{flood_protection_name}"
-        probability_columns = [str(p) for p in probabilities]
-
-    dataframe.columns = dataframe.columns.astype(str)
-    dataframe[expected_risk_column] = list(
-        integrate.trapezoid(
-            dataframe[probability_columns].to_numpy(),
-            np.array([probabilities * len(dataframe.index)]).reshape(
-                dataframe[probability_columns].shape
-            ),
-        )
-    )
-
-    return dataframe[index_columns + [expected_risk_column]]
