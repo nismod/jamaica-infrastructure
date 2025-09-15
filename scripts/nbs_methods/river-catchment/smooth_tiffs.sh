f7fbae5c483e90baaca62b9240862cfe81358013
Tom Russell <tom.russell@ouce.ox.ac.uk>
2025-09-15 15:15:31 +0100

    Coarsen and smooth avoided damages TIFFs

--- scripts/nbs_methods/river-catchment/smooth_tiffs.sh
+++ scripts/nbs_methods/river-catchment/smooth_tiffs.sh
@@ -25,3 +25,15 @@ python workflow/4b_hotspots/kde.py \
     -i processed_data/nbs-river-catchment/damage_reduction_max.tif \
     -o processed_data/nbs-river-catchment/damage_reduction_max_smoothed.tif \
     -b 300 # m
+
+#
+# Coarsen then smooth avoided damages
+#
+gdalwarp -overwrite -t_srs EPSG:3448 processed_data/nbs-river-catchment/avoided__fluvial__ead_min.tif processed_data/nbs-river-catchment/avoided__fluvial__ead_min_3448.tif
+gdalwarp -overwrite -t_srs EPSG:3448 processed_data/nbs-river-catchment/avoided__fluvial__ead_max.tif processed_data/nbs-river-catchment/avoided__fluvial__ead_max_3448.tif
+
+gdalwarp -overwrite -r sum -tr 300 300 -t_srs EPSG:3448 processed_data/nbs-river-catchment/avoided__fluvial__ead_min_3448.tif processed_data/nbs-river-catchment/avoided__fluvial__ead_min_300m.tif
+gdalwarp -overwrite -r sum -tr 300 300 -t_srs EPSG:3448 processed_data/nbs-river-catchment/avoided__fluvial__ead_max_3448.tif processed_data/nbs-river-catchment/avoided__fluvial__ead_max_300m.tif
+
+python workflow/4b_hotspots/kde.py -i processed_data/nbs-river-catchment/avoided__fluvial__ead_min_300m.tif -o processed_data/nbs-river-catchment/avoided__fluvial__ead_min_300m_smoothed.tif -b 300
+python workflow/4b_hotspots/kde.py -i processed_data/nbs-river-catchment/avoided__fluvial__ead_max_300m.tif -o processed_data/nbs-river-catchment/avoided__fluvial__ead_max_300m_smoothed.tif -b 300
