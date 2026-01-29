cb98fe64b6e0e378d2bfc6e082f834ec33df60c9
Fred Thomas <fred.thomas@eci.ox.ac.uk>
2026-01-29 15:56:40 +0000

    Rewrite as async

--- workflow/utilities/check_targets.py
+++ workflow/utilities/check_targets.py
@@ -7,14 +7,10 @@ It does not try and generate any of the intermediate or target files.
 
 Run from the root of the repository:
 $ python workflow/utilities/check_targets.py
-
-If .tmp already exists, it will need to be deleted first.
 """
 
-import os
-import subprocess
-import re
-import sys
+import asyncio
+import logging
 
 
 EXPOSURE = [
@@ -246,52 +242,42 @@ HOTSPOTS = [
     "results/hotspots/transport/rerouting_loss_smoothed.tiff",
 ]
 
-TARGETS = EXPOSURE + DAMAGES + LOSSES + EAD_EAEL + ADAPTATIONS + HOTSPOTS
+METADATA = [
+    "processed_data/networks_uids/network_layers.csv",
+]
+
+TARGETS = EXPOSURE + DAMAGES + LOSSES + EAD_EAEL + ADAPTATIONS + HOTSPOTS + METADATA
+
+
+async def check_target(target: str) -> bool:
+    """Check if a single target file can be built."""
+
+    proc = await asyncio.create_subprocess_shell(
+        f"snakemake --dry-run --cores 1 --nolock {target}",
+        stdout=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.PIPE,
+    )
+    stdout, stderr = await proc.communicate()
+
+    if (success := proc.returncode == 0):
+        logging.info(f"Pass {target}")
+    else:
+        logging.debug("FAILED:")
+        logging.debug(stdout.decode())
+        logging.debug(stderr.decode())
+        logging.info(f"Fail {target}")
+
+    return success
+
+
+async def main():
+    """Check all targets asynchronously."""
+
+    logging.basicConfig(format="%(asctime)s %(process)d %(filename)s %(message)s", level=logging.INFO)
+
+    results = await asyncio.gather(*[check_target(target) for target in TARGETS])
+    logging.info(f"{sum(results)}/{len(TARGETS)} targets pass dry-run test.")
+
 
 if __name__ == "__main__":
-    n_okay = 0
-    n_warn = 0
-
-    if os.path.exists(".tmp"):
-        sys.exit("Please remove .tmp folder before running.")
-    else:
-        os.makedirs(".tmp/rulegraphs")
-
-    for target in TARGETS:
-
-        # run snakemake to check the rulegraph
-        # print the output if it fails
-        print(f"Checking {target}... ", end="")
-        run = subprocess.run(
-            f"snakemake --cores 1 --rulegraph {target}",
-            capture_output=True,
-            shell=True,
-        )
-
-        if run.returncode == 0:
-            n_okay += 1
-            matches = re.findall("(Warning: .+)", run.stdout.decode(), re.IGNORECASE)
-            if len(matches) > 0:
-                print(f"{len(matches)} warnings:")
-                print("\n".join(matches))
-                n_warn += 1
-            else:
-                fname = os.path.basename(target).replace('.parquet', '.png').replace('.csv', '.png')
-
-                # plot the rulegraph and write to disk
-                subprocess.run(
-                    ["dot", "-Tpng", "-o", f".tmp/rulegraphs/{fname}"],
-                    check=True,
-                    input=run.stdout,
-                )
-
-                print("OK")
-        else:
-            print("FAILED:")
-            print(run.stdout.decode())
-            print(run.stderr.decode())
-
-    if n_okay == len(TARGETS):
-        print("All targets pass dry-run test.")
-    else:
-        print(f"{n_okay}/{len(TARGETS)} targets pass dry-run test.")
+    asyncio.run(main())
